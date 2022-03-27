9dbafcd9205db5e7d39932160ad16776388fee24
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-03-27 01:14:34 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,25 +6,24 @@
  * Copyright: El_isra (https://www.github.com/israpps)
  * License:   GPL-3.0
  **************************************************************/
+
 #include <iostream>
 #include <string.h>
+
 #include "BinMerger.h"
-using namespace std;
 #include "version.h"
+
+using namespace std;
+
 int main(int argc, char** argv)
 {
-    std::string ARGV0 = argv[0], mrg;
+    std::string ARGV0 = argv[0];
     std::cout <<"\tBinMerger "<<AutoVersion::FULLVERSION_STRING<<"\n"<<
                 "\tCoded By Matias Israelson (El_isra)  [www.github.com/israpps]\n";
-    if (argc < 2) {std::cerr << "not enough argumments\nUsage:\n\t"<<ARGV0.substr(ARGV0.find_last_of('\\')+1)<<" [CUE Location] [New Filename (without extension)]\n";return -1;}
-    if (argc== 2)
-    {
-        mrg = std::string(argv[1]);
-        mrg += "_merged";
-    }
+    if (argc < 3) {std::cerr << "not enough argumments\nUsage:\n\t"<<ARGV0.substr(ARGV0.find_last_of('\\')+1)<<" [CUE Location] [New Filename (without extension)]\n";return -1;}
     BinMerger A;
     std::vector<bin_t> B = A.parse_cue(std::string(argv[1]));
     A.generate_merged_cue(B,std::string(argv[2]));
-    A.fuse_bins(B, (argc < 3) ? std::string(argv[2]) : mrg);
+    A.fuse_bins(B,std::string(argv[2]));
     return 0;
 }
