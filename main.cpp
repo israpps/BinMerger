f350fc68ea0ed3865f5923a9833832cf6db1231d
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-03-27 04:18:42 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
     if (argc < 3) {std::cerr << "not enough argumments\nUsage:\n\t"<<ARGV0.substr(ARGV0.find_last_of('\\')+1)<<" [CUE Location] [New Filename (without extension)]\n";return -1;}
     BinMerger A;
     std::vector<bin_t> B = A.parse_cue(std::string(argv[1]));
-	if (B.size() == 0 ) {std::cerr << "Collected data from BIN was incorrect or incomplete...\naborting file merging...\n"; return -1:}
+	if (B.size() == 0 ) {std::cerr << "Collected data from BIN was incorrect or incomplete...\naborting file merging...\n"; return -1;}
     A.generate_merged_cue(B,std::string(argv[2]));
     A.fuse_bins(B,std::string(argv[2]));
     return 0;
