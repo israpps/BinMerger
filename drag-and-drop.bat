8bf354f81af55b4aa6398931ab478f07e2920984
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-03-27 21:24:57 -0300

    Update drag-and-drop.bat

--- drag-and-drop.bat
+++ drag-and-drop.bat
@@ -3,5 +3,5 @@ CD /d "%~dp0"
 echo selected file: [%1]
 echo please write new filename (without extension) and press enter
 SET /P NAME=
-BinMerger.exe "%1" "%NAME%"
+BinMerger.exe "%~1" "%NAME%"
 Pause
